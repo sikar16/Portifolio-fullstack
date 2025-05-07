4025e9c93864f46b395e46302d3e13baedf8fd34
sikar16 <sikarmagi6123@gmail.com>
2025-05-07 12:41:59 +0300

    update the origin

--- Backend/app.js
+++ Backend/app.js
@@ -8,11 +8,12 @@ import { PORT, HOST } from "./src/config/secret.js";
 const app = express();
 
 app.use(cors({
-    origin: '*', 
+    origin: true, 
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true
 }));
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
