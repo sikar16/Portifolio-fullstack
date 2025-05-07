6546d6190a848ecca19f8c75eedf2c5bf6516df1
sikar16 <sikarmagi6123@gmail.com>
2025-05-07 12:27:26 +0300

    change the origin

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ import { PORT, HOST } from "./src/config/secret.js";
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: '*', 
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true
